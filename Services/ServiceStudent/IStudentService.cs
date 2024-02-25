79a7d46b8116fe07e14ca9bc54f42cbd6a0aaa20
JayvonK <jaykhuth@gmail.com>
2024-02-24 23:21:09 -0800

    interface

---
+++ Services/ServiceStudent/IStudentService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace studentInfoHubAPI.Services
+{
+    public class Student{
+        public string StudentName {get; set;} = "No Student";
+        public string StudentGrade {get; set;} = "N/A";
+    }
+    public interface IStudentService
+    {
+        
+    }
+}
