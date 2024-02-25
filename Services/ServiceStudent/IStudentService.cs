e1e76b72e5e92c727a53e9f79ce7084637a96b65
JayvonK <jaykhuth@gmail.com>
2024-02-24 23:25:09 -0800

    save

--- Services/ServiceStudent/IStudentService.cs
+++ Services/ServiceStudent/IStudentService.cs
@@ -1,16 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace studentInfoHubAPI.Services
-{
+namespace studentInfoHubAPI.Services;
+
     public class Student{
         public string StudentName {get; set;} = "No Student";
         public string StudentGrade {get; set;} = "N/A";
     }
     public interface IStudentService
     {
-        
+        List<Student> GetAllStudents();
+
+        Student SearchStudent(string studentName);
+
+        List<Student> UpdateStudent(string studentName, string newName, string newGrade);
+
+        List<Student> DeleteStudent(string studentName);
+
+        List<Student> AddStudent(string studentName, string studentGrade);
+
     }
-}
