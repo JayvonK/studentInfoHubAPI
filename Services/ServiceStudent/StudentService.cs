38b5a8799255c2fa509385929b2e0c7ca1ddb2a8
JayvonK <jaykhuth@gmail.com>
2024-02-25 20:51:38 -0800

    student class

--- Services/ServiceStudent/StudentService.cs
+++ Services/ServiceStudent/StudentService.cs
@@ -1,12 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace studentInfoHubAPI.Services.ServiceStudent
+namespace studentInfoHubAPI.Services.ServiceStudent;
+
+public class StudentService : IStudentService
 {
-    public class StudentService
+    public List<Student> AddStudent(string studentName, string studentGrade)
     {
-        
+        throw new NotImplementedException();
+    }
+
+    public List<Student> DeleteStudent(string studentName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Student> GetAllStudents()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Student SearchStudent(string studentName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Student> UpdateStudent(string studentName, string newName, string newGrade)
+    {
+        throw new NotImplementedException();
     }
 }
